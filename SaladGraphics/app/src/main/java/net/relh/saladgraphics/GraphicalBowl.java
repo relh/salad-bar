ba6807346a0fe01c529e60ac4d7d63bca3c5a569
relh <richard@relh.net>
2014-11-30 15:54:46 -0500

    added graphics as well as portion of assembley view

--- SaladGraphics/app/src/main/java/net/relh/saladgraphics/GraphicalBowl.java
+++ SaladGraphics/app/src/main/java/net/relh/saladgraphics/GraphicalBowl.java
@@ -1,9 +1,20 @@
 package net.relh.saladgraphics;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class GraphicalBowl extends Activity {
@@ -12,6 +23,86 @@ public class GraphicalBowl extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_graphical_bowl);
+
+        // LISTVIEWS
+        final ListView baseList = (ListView) findViewById(R.id.base);
+        final ListView proteinList = (ListView) findViewById(R.id.protein);
+        final ListView toppingList = (ListView) findViewById(R.id.toppings);
+
+        // Defined Array values to show in ListView
+        String[] bases = new String[]{
+                "spinach",
+                "lettuce"
+        };
+        String[] proteins = new String[]{
+                "spinach",
+                "lettuce"
+        };
+        String[] toppings = new String[]{
+                "broccoli",
+                "mushrooms",
+                "olives",
+                "tomatoes",
+                "avocado",
+                "carrots",
+                "corn",
+                "onions",
+                "cucumber",
+                "peppers",
+                "oranges",
+                "apples"
+        };
+
+        ArrayAdapter<String> baseAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1, bases);
+        ArrayAdapter<String> proteinAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1, proteins);
+        ArrayAdapter<String> toppingAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1, toppings);
+
+        // Assign adapter to ListView
+        baseList.setAdapter(baseAdapter);
+        proteinList.setAdapter(proteinAdapter);
+        toppingList.setAdapter(toppingAdapter);
+
+        // Add headers
+        TextView baseHeader = (TextView) findViewById(R.id.baseHeader);
+        baseHeader.setText("Bases:");
+        baseHeader.setBackgroundColor(Color.RED);
+
+        TextView proteinHeader = (TextView) findViewById(R.id.proteinHeader);
+        proteinHeader.setText("Protein:");
+        proteinHeader.setBackgroundColor(Color.GREEN);
+
+        TextView toppingHeader = (TextView) findViewById(R.id.toppingHeader);
+        toppingHeader.setText("Toppings:");
+        toppingHeader.setBackgroundColor(Color.BLUE);
+
+        // ListView Item Click Listener
+        baseList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+
+                // ListView Clicked item index
+                int itemPosition     = position;
+
+                // ListView Clicked item value
+                String  itemValue    = (String) baseList.getItemAtPosition(position);
+
+                // Show Alert
+                Toast.makeText(getApplicationContext(),
+                        "Position :" + itemPosition + "  ListItem : " + itemValue, Toast.LENGTH_LONG)
+                        .show();
+
+            }
+
+        });
+
+        // IMAGES
+        LinearLayout layout = (LinearLayout) findViewById(R.id.graphicLayout);
+
     }
 
 
