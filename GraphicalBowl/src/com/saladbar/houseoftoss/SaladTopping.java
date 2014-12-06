111c99cc62a08f4b546b6d8ea51a0ad73b665461
Brandon Hoffman <brandon.michael.hoffman@gmail.com>
2014-12-06 11:23:58 -0500

    merged

---
+++ GraphicalBowl/src/com/saladbar/houseoftoss/SaladTopping.java
@@ -0,0 +1,19 @@
+package com.saladbar.houseoftoss;
+
+public class SaladTopping {
+    private String name;
+    private String imageSrc;
+    
+    public SaladTopping(String name, Float price, String src){
+    	this.name = name;
+    	this.imageSrc = src;
+    }
+    
+    public String getName(){
+    	return this.name;
+    }
+    
+    public String getImgSrc(){
+    	return this.imageSrc;
+    }
+}
