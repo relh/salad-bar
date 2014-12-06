88d7765ae016c1e9bfe7d85d89f1b7b6ed48ece9
Sheng Shen <sshengshen@gmail.com>
2014-12-06 12:28:10 -0500

    Create SplashActivity.java

---
+++ GraphicalBowl/src/com/saladbar/houseoftoss/SplashActivity.java
@@ -0,0 +1,12 @@
+package net.relh.saladgraphics;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class SplashActivity extends Activity {
+
