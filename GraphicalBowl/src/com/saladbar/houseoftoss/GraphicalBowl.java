a28983649134b348c0bdece675a70b1e0af9a268
relh <richard@relh.net>
2014-12-06 16:15:46 -0500

    added graphical view

--- GraphicalBowl/src/com/saladbar/houseoftoss/GraphicalBowl.java
+++ GraphicalBowl/src/com/saladbar/houseoftoss/GraphicalBowl.java
@@ -3,7 +3,11 @@ package com.saladbar.houseoftoss;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
 import android.graphics.drawable.AnimationDrawable;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
@@ -13,10 +17,13 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.media.Image;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.AdapterView;
@@ -26,12 +33,16 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-
+import java.util.Random;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
 
 public class GraphicalBowl extends Activity implements SensorEventListener {
 
@@ -42,6 +53,13 @@ public class GraphicalBowl extends Activity implements SensorEventListener {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_graphical_bowl);
+        
+        // GRAPHICAL VIEW
+		RelativeLayout relativeLayout = (RelativeLayout) findViewById(R.id.graphicLayout);
+		final ToppingView bubbleView = new ToppingView(getApplicationContext(),
+				BitmapFactory.decodeResource(getResources(), R.drawable.broccoli));
+
+		relativeLayout.addView(bubbleView);
         
         // LISTVIEWS
         final ListView baseList = (ListView) findViewById(R.id.base);
@@ -126,7 +144,7 @@ public class GraphicalBowl extends Activity implements SensorEventListener {
         items = new HashMap<String, ImageView>();
         
         // IMAGES
-        layout = (GridLayout) findViewById(R.id.graphicLayout);
+        //layout = (GridLayout) findViewById(R.id.graphicLayout);
 
         // Add spoken toppings
         Intent intent = getIntent();
@@ -206,7 +224,7 @@ public class GraphicalBowl extends Activity implements SensorEventListener {
     private float x, y, z, last_x, last_y, last_z;
     long curTime;
     long lastUpdate = 0;
-    boolean shakeStarted = false;
+    static boolean shakeStarted = false;
     
     // Shakes insta close the activity
     @Override
@@ -308,5 +326,100 @@ public class GraphicalBowl extends Activity implements SensorEventListener {
             //rocketAnimation = (AnimationDrawable) rocketImage.getBackground();
         }
     }
-
+    
+    private class ToppingView extends SurfaceView implements
+	SurfaceHolder.Callback {
+		
+		private final Bitmap mBitmap;
+		private final int mBitmapHeightAndWidth, mBitmapHeightAndWidthAdj;
+		private final DisplayMetrics mDisplay;
+		private final int mDisplayWidth, mDisplayHeight;
+		private float mX, mY, mDx, mDy, mRotation;
+		private final SurfaceHolder mSurfaceHolder;
+		private final Paint mPainter = new Paint();
+		private Thread mDrawingThread;
+		
+		private static final int MOVE_STEP = 1;
+		private static final float ROT_STEP = 1.0f;
+		
+		public ToppingView(Context context, Bitmap bitmap) {
+			super(context);
+		
+			mBitmapHeightAndWidth = (int) getResources().getDimension(
+					R.dimen.image);
+			this.mBitmap = Bitmap.createScaledBitmap(bitmap,
+					mBitmapHeightAndWidth, mBitmapHeightAndWidth, false);
+		
+			mBitmapHeightAndWidthAdj = mBitmapHeightAndWidth / 2;
+		
+			mDisplay = new DisplayMetrics();
+			GraphicalBowl.this.getWindowManager().getDefaultDisplay()
+					.getMetrics(mDisplay);
+			mDisplayWidth = mDisplay.widthPixels;
+			mDisplayHeight = mDisplay.heightPixels;
+		
+			Random r = new Random();
+			mX = (float) r.nextInt(mDisplayHeight);
+			mY = (float) r.nextInt(mDisplayWidth);
+			mDx = (float) r.nextInt(mDisplayHeight) / mDisplayHeight;
+			mDx *= r.nextInt(2) == 1 ? MOVE_STEP : -1 * MOVE_STEP;
+			mDy = (float) r.nextInt(mDisplayWidth) / mDisplayWidth;
+			mDy *= r.nextInt(2) == 1 ? MOVE_STEP : -1 * MOVE_STEP;
+			mRotation = 1.0f;
+		
+			mPainter.setAntiAlias(true);
+		
+			mSurfaceHolder = getHolder();
+			mSurfaceHolder.addCallback(this);
+		}
+		
+		private void drawBubble(Canvas canvas) {
+			canvas.drawColor(Color.DKGRAY);
+			mRotation += ROT_STEP;
+			canvas.rotate(mRotation, mY + mBitmapHeightAndWidthAdj, mX
+					+ mBitmapHeightAndWidthAdj);
+			canvas.drawBitmap(mBitmap, mY, mX, mPainter);
+		}
+		
+		private boolean move() {
+			mX += mDx;
+			mY += mDy;
+			if (mX < 0 - mBitmapHeightAndWidth
+					|| mX > mDisplayHeight + mBitmapHeightAndWidth
+					|| mY < 0 - mBitmapHeightAndWidth
+					|| mY > mDisplayWidth + mBitmapHeightAndWidth) {
+				return false;
+			} else {
+				return true;
+			}
+		}
+		
+		@Override
+		public void surfaceChanged(SurfaceHolder holder, int format, int width,
+				int height) {
+		}
+		
+		@Override
+		public void surfaceCreated(SurfaceHolder holder) {
+			mDrawingThread = new Thread(new Runnable() {
+				public void run() {
+					Canvas canvas = null;
+					while (!Thread.currentThread().isInterrupted() && move()) {
+						canvas = mSurfaceHolder.lockCanvas();
+						if (null != canvas) {
+							drawBubble(canvas);
+							mSurfaceHolder.unlockCanvasAndPost(canvas);
+						}
+					}
+				}
+			});
+			mDrawingThread.start();
+		}
+		
+		@Override
+		public void surfaceDestroyed(SurfaceHolder holder) {
+			if (null != mDrawingThread)
+				mDrawingThread.interrupt();
+		}
+	}   
 }
