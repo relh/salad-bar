3f6000e42b4ed7413ffcb37e501dcd617a514dca
Sheng Shen <sshengshen@gmail.com>
2014-12-06 12:30:18 -0500

    Update SplashActivity.java

--- GraphicalBowl/src/com/saladbar/houseoftoss/SplashActivity.java
+++ GraphicalBowl/src/com/saladbar/houseoftoss/SplashActivity.java
@@ -1,12 +1,33 @@
 package net.relh.saladgraphics;
 
 import android.app.Activity;
-import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
+
 
 public class SplashActivity extends Activity {
 
+    private static int TIME = 2000;
+    private static String TAG = "Salad-Bar";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Log.i(TAG,
+                        "Entered splash page");
+                
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, TIME);
+    }
+}
