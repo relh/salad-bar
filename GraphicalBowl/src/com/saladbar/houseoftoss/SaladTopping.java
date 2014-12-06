494fc4faff217792aa671b87a7ca1fdf0a852dcd
Sheng Shen <sshengshen@gmail.com>
2014-12-06 14:12:45 -0500

    Update SaladTopping.java
    add price and calories functions

--- GraphicalBowl/src/com/saladbar/houseoftoss/SaladTopping.java
+++ GraphicalBowl/src/com/saladbar/houseoftoss/SaladTopping.java
@@ -3,10 +3,14 @@ package com.saladbar.houseoftoss;
 public class SaladTopping {
     private String name;
     private String imageSrc;
+    private Float price;
+    private Float calories;
     
-    public SaladTopping(String name, Float price, String src){
+    public SaladTopping(String name, Float price, Float calories, String src){
     	this.name = name;
     	this.imageSrc = src;
+    	this.price = price;
+    	this.calories = calories;
     }
     
     public String getName(){
@@ -15,5 +19,13 @@ public class SaladTopping {
     
     public String getImgSrc(){
     	return this.imageSrc;
+    }
+    
+    public Float getPrice() {
+        return this.price;
+    }
+    
+    public Float getCalories() {
+        return this.calories;
     }
 }
